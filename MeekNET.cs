3f126ae8b39b2a06ae1ce4c8dc4f9ca094bbd0f4
Benjamin K <speyd3r@srgg.de>
2019-03-01 06:41:45 +0100

    i cant spell shit lmao

--- MeekNET.cs
+++ MeekNET.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using HeyRed.Mime;
+using MeekNET.Entities;
+using MeekNET.Enums;
+using Newtonsoft.Json;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using HeyRed.Mime;
-using MeekNET.Enums;
-using MeekNET.Entities;
 
 namespace MeekNET
 {
@@ -42,7 +41,7 @@ namespace MeekNET
             return iu;
         }
 
-        public async Task<ImgUrl> GetImgURL(Loids loid)
+        public async Task<ImgUrl> GetImgURL(Loids loid, int size = 500)
         {
             WebClient wc = new WebClient();
             string ApiJson = await wc.DownloadStringTaskAsync($"https://api.meek.moe/{GetLoid(loid)}");
@@ -50,7 +49,7 @@ namespace MeekNET
             ImgUrl iu = new ImgUrl
             {
                 Url = ar.Url,
-                ProxyUrl = $"https://api.meek.moe/im/?image={ar.Url}&resize=500",
+                ProxyUrl = $"https://api.meek.moe/im/?image={ar.Url}&resize={size}",
                 Creator = ar.Creator
             };
             return iu;
@@ -62,87 +61,102 @@ namespace MeekNET
             {
                 case Enums.Loids.ProjectDiva:
                     return "diva";
+
                 case Enums.Loids.KagamineRin:
                     return "rin";
+
                 case Enums.Loids.OtomachiUna:
                     return "una";
+
                 case Enums.Loids.Gumi:
                     return "gumi";
+
                 case Enums.Loids.MegurineLuka:
                     return "luka";
+
                 case Enums.Loids.IA:
                     return "ia";
+
                 case Enums.Loids.Fukase:
                     return "fukase";
+
                 case Enums.Loids.HatsuneMiku:
                     return "miku";
+
                 case Enums.Loids.KagamineLen:
                     return "len";
+
                 case Enums.Loids.Kaito:
                     return "kaito";
+
                 case Enums.Loids.KasaneTeto:
                     return "teto";
+
                 case Enums.Loids.Meiko:
                     return "meiko";
+
                 case Enums.Loids.YuzukiYukari:
                     return "yukari";
+
                 case Enums.Loids.SFA2Miki:
                     return "miki";
+
                 case Enums.Loids.Lily:
                     return "lily";
+
                 case Enums.Loids.Mayu:
                     return "mayu";
+
                 case Enums.Loids.AokiLapis:
                     return "aoki";
-
             }
             return null;
         }
 
         private class ApiResponse
         {
-            public string Url { get; set; }
             public string Creator { get; set; }
+            public string Url { get; set; }
         }
+    }
+}
+
+namespace MeekNET.Entities
+{
+    public class ImgData : ImgUrl
+    {
+        public Stream Image { get; set; }
+    }
+
+    public class ImgUrl
+    {
+        public string Creator { get; set; }
+        public string FileType { get; set; }
+        public string ProxyUrl { get; set; }
+        public string Url { get; set; }
     }
 }
 
 namespace MeekNET.Enums
 {
-      public enum Loids
-        {
-            ProjectDiva,
-            KagamineRin,
-            OtomachiUna,
-            Gumi,
-            MegurineLuka,
-            IA,
-            Fukase,
-            HatsuneMiku,
-            KagamineLen,
-            Kaito,
-            KasaneTeto,
-            Meiko,
-            YuzukiYukari,
-            SFA2Miki,
-            Lily,
-            Mayu,
-            AokiLapis
-        }
-}
-
-namespace MeekNET.Entities
-{
-    public class ImgUrl
+    public enum Loids
     {
-        public string Url { get; set; }
-        public string ProxyUrl { get; set; }
-        public string Creator { get; set; }
-        public string FileType { get; set; }
-    }
-
-    public class ImgData : ImgUrl
-    {
-        public Stream Image { get; set; }
+        ProjectDiva,
+        KagamineRin,
+        OtomachiUna,
+        Gumi,
+        MegurineLuka,
+        IA,
+        Fukase,
+        HatsuneMiku,
+        KagamineLen,
+        Kaito,
+        KasaneTeto,
+        Meiko,
+        YuzukiYukari,
+        SFA2Miki,
+        Lily,
+        Mayu,
+        AokiLapis
     }
 }
